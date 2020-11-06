4193ae20c7babbdd4e0213872dc827a26bd303e4
Victor Dantas <victor@rogalabs.com>
2020-11-05 21:29:25 -0300

    chore: install vue-mask

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueTheMask from 'vue-the-mask'
+
+Vue.use(VueTheMask)
 
 Vue.config.productionTip = false
 
