fb8e14efe084c7a5df3b222f9b67d402310f3583
Victor Dantas <victor@rogalabs.com>
2020-11-12 06:24:55 -0300

    feat: create default Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/ic-logo.svg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container 
+    class="home pa-4" 
+    fluid
+  >
+    
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
