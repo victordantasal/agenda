287cb82337e1dfeb3551f90eb2df4b476c49a703
Victor Dantas <victor@rogalabs.com>
2020-11-12 06:12:12 -0300

    feat: define Application routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '*',
+    redirect: {name: 'Root'}
   },
+  
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'Root',
+    component: () => import('../layouts/DefaultLayout.vue'),
+    redirect: { name: 'Home' },
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+      }
+    ]
   }
 ]
 
