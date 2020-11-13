563f0373a73f5dbc26cde16341e18219a8c18553
Victor Dantas <victor@rogalabs.com>
2020-11-13 03:20:40 -0300

    feat: add some overrides on vuetify(simple way)

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,24 @@
 <style>
   #app {
     background-color: #f8f9fd;
+    color: #2a2d3b !important;
     font-family: 'Roboto';
+    font-weight: normal !important;
+    font-stretch: normal !important;
+    font-style: normal !important;
+    line-height: normal !important;
+    letter-spacing: normal !important;
+  }
+
+  img {
+    display: block;
   }
   
-  input:focus, button:focus { outline:none }
+  input:focus, button:focus { 
+    outline: none;
+  }
+
+  .v-dialog {
+    overflow-y: initial !important;
+  }
 </style>
