70fa6acd93d7104fab706807bd8f609dc9e92f66
Victor Dantas <victor@rogalabs.com>
2020-11-13 03:25:30 -0300

    feat: add update and create dialog on Layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,13 +4,18 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+    <ContactUpdateDialog />
+    <ContactCreateDialog />
   </v-app>
 </template>
 
 <script>
 import Topbar from '@/components/layout/Topbar.vue'
+import ContactCreateDialog from '@/components/ContactCreateDialog.vue'
+import ContactUpdateDialog from '@/components/ContactUpdateDialog.vue'
+
 export default {
-  components: { Topbar }
+  components: { Topbar, ContactCreateDialog, ContactUpdateDialog }
 }
 </script>
 
