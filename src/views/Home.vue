b9df998a222ba3f289984de78b04b9ba476469ed
Victor Dantas <victor@rogalabs.com>
2020-11-13 03:26:50 -0300

    feat: add contactList on Home View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,16 @@
     class="home pa-4" 
     fluid
   >
-    
+    <ContactsList />
+
   </v-container>
 </template>
 
 <script>
+import ContactsList from '@/components/ContactsList.vue'
 
 export default {
+  components: { ContactsList }
   
 }
 </script>
